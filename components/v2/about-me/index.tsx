2862e7f1012efc0e9e7dd52dd45d01012763df4c
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-09 14:05:52 -0300

    centralizando as imagens

--- components/v2/about-me/index.tsx
+++ components/v2/about-me/index.tsx
@@ -6,7 +6,7 @@ export class AboutMe extends React.Component {
     render(): React.ReactNode {
         return (
             <Container className="col-xxl-8 px-4 py-5">
-                <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div className="row flex-lg-row-reverse align-items-center justify-content-center g-5 py-5">
                     <div className="col-10 col-sm-8 col-lg-6">
                         <img src={img.src} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
                     </div>
--- components/v2/feature-projects/index.tsx
+++ components/v2/feature-projects/index.tsx
@@ -15,7 +15,7 @@ export class CardFeatureProject extends React.Component<FeatureProject> {
     render(): React.ReactNode {
         const { imagesrc, description, title, link, languages } = this.props;
         return (
-            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div className="row flex-lg-row-reverse align-items-center justify-content-center g-5 py-5">
                 <div className="col-10 col-sm-8 col-lg-6">
                     <img src={imagesrc} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
                 </div>
