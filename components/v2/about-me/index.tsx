341e89f609e409f03fb6bc4abc64843ea66150b0
Jorge-Calheiros-de-Sousa <jorgecalheiros.s@gmail.com>
2023-01-10 00:25:12 -0300

    text about me

--- components/v2/about-me/index.tsx
+++ components/v2/about-me/index.tsx
@@ -13,7 +13,10 @@ export class AboutMe extends React.Component {
                     <div className="col-lg-6">
                         <h1 className="display-2 fw-bold lh-1 mb-3">Jorge Calheiros</h1>
                         <p className="lead">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            Olá eu sou desolvolvedor web fullstack tenho 19 anos, sou apaixonado por ténologias e sempre busco aprender mais sobre a área.
+                            <br />
+
+                            <strong>jorgecalheiros.s@gmail.com</strong>
                         </p>
                     </div>
                 </div>
